6df569535949a552c65f9dfe4e78899c73a0947a
radim.nedved <nedvedradim1@gmail.com>
2023-11-28 19:27:55 +0100

    tile sets

---
+++ tile-sets.js
@@ -0,0 +1,19 @@
+const set1 = [
+  [
+    ["white", "white", "white"],
+    ["black", "black", "black"],
+    ["white", "white", "white"],
+  ],
+  [
+    ["white", "black", "white"],
+    ["black", "black", "white"],
+    ["white", "white", "white"],
+  ],
+  [
+    ["black", "white", "white"],
+    ["black", "white", "white"],
+    ["black", "black", "black"],
+  ],
+];
+
+export { set1 };
