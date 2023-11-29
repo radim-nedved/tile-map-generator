d1fb172357dc81484256f3a7cceb83bf5296fcf7
radim.nedved <nedvedradim1@gmail.com>
2023-11-29 11:33:52 +0100

    tile validation

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 import { set1 } from "./tile-sets.js";
 
 let tileSet = set1;
-let gridWidth = 3;
-let tileSize = 50;
+let gridWidth = 10;
+let tileSize = 30;
 let grid = [];
 
 for (let i = 0; i < gridWidth; i++) {
   grid.push([]);
   for (let j = 0; j < gridWidth; j++) {
-    grid[i].push(tileSet[0]);
+    if (isTileValid(tileSet[0], j, i, tileSet[0].length)) {
+      grid[i].push(tileSet[0]);
+    }
   }
 }
 
 drawGrid();
+
+function isTileValid(tile, x, y, width) {
+  if (y > 0) {
+    const top = grid[y - 1][x];
+
+    if (top) {
+      for (let i = 0; i < width; i++) {
+        if (top[width - 1][i] !== tile[0][i]) {
+          return false;
+        }
+      }
+    }
+  }
+
+  if (x > 0) {
+    const left = grid[y][x - 1];
+
+    if (left) {
+      for (let i = 0; i < width; i++) {
+        if (left[i][width - 1] !== tile[i][0]) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+}
 
 function drawGrid() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
 
   const tileWidth = grid[0][0].length;
+
+  canvas.width = gridWidth * tileSize;
+  canvas.height = gridWidth * tileSize;
 
   for (let i = 0; i < gridWidth; i++) {
     for (let j = 0; j < gridWidth; j++) {
@@ -35,4 +68,6 @@ function drawGrid() {
       }
     }
   }
+
+  document.getElementById("grid-img").src = canvas.toDataURL();
 }
